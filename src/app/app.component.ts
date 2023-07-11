d0db6859920f95e1c80b2455e8ec595c7457c48c
Joshua Daniel <joshua_dup@hotmail.com>
2023-07-11 10:55:29 -0500

    directivass ngIf y ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'trabajoDirectivasApp';
+  title = 'Registro de Usuarios';
+  mensaje = '';
+  userRegistrado = false;
+  nombre: string = '';
+  apellido: string = '';
+  entradas: any;
+
+  constructor() {
+    this.entradas = [
+      { titulo: 'Python cada día más presente' },
+      { titulo: 'Java presente desde hace mas de 20 años' },
+      { titulo: 'Kotlin potencia para tus apps' },
+      { titulo: 'JavaScript cada vez mas funcional' },
+      { titulo: '¿Angular el framework mas completo?' },
+    ];
+  }
+
+  registrarUsuario() {
+    this.userRegistrado = true;
+    this.mensaje = 'Usuario registrado con éxito';
+  }
 }
